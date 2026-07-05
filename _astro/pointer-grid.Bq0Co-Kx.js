0526894c7f397db0760d60f3ad802cba4bea5c9c
Alex <190883354+alex717raw@users.noreply.github.com>
2026-07-05 14:15:26 +0300

    Deploy AmiSoft site

---
+++ _astro/pointer-grid.Bq0Co-Kx.js
@@ -0,0 +1,1 @@
+const t=document.documentElement,o=e=>{t.style.setProperty("--mouse-x",`${e.clientX}px`),t.style.setProperty("--mouse-y",`${e.clientY}px`)};window.addEventListener("pointermove",o,{passive:!0});
